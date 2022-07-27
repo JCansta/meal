424e437384bc41e74a1676bc317db5b52f9f3f51
JCansta <jaime.castro03@icloud.com>
2022-07-27 01:09:08 -0500

    upgrade ruby version

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
     has_secure_password
     has_many :claps, dependent: :destroy
 
-    validates :email, format: {with: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$, message: 'solo formato email'}
 end
--- spec/requests/recipes_request_spec.rb
+++ spec/requests/recipes_request_spec.rb
@@ -4,3 +4,6 @@ RSpec.describe "Recipes", type: :request do
     
     describe "GET /api/v1/recipes" do
         context 'succesful recipe' do
+        end
+    end
+end
